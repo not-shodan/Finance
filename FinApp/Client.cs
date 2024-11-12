bd1b60f36b79e4df8f641afb9a94c6eee20ef53f
sharptooth <diego.educr@gmail.com>
2024-11-12 11:23:52 -0300

    Update Client.cs

--- FinApp/Client.cs
+++ FinApp/Client.cs
@@ -19,3 +19,49 @@ public class Client
         LocalPortfolios = new List<LocalPortfolio>();
     }
 }
+
+
+
+
+
+
+
+
+
+
+using System;
+
+public class Client
+{
+    // Attributes
+    public int NoClient { get; set; }
+    public string NameClient { get; set; }
+    public int NoPortfolios { get; set; }
+    public double TotalPosition { get; set; }
+    public int RiskScore { get; set; }
+
+    // Constructor to initialize the attributes
+    public Client(int noClient, string nameClient, int noPortfolios, double totalPosition, int riskScore)
+    {
+        NoClient = noClient;
+        NameClient = nameClient;
+        NoPortfolios = noPortfolios;
+        TotalPosition = totalPosition;
+        RiskScore = riskScore;
+    }
+
+    // Function to display the client details
+    public void DisplayClientDetails()
+    {
+        Console.WriteLine("Client No: " + NoClient.ToString("D3"));
+        Console.WriteLine("Name: " + NameClient);
+        Console.WriteLine("Number of Portfolios: " + NoPortfolios);
+        Console.WriteLine("Total Position: " + TotalPosition.ToString("C"));
+        Console.WriteLine("Risk Score: " + RiskScore);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
