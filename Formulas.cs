f01f19d4f849137a83625e10efe544a88e54cf3a
sharptooth <diego.educr@gmail.com>
2024-11-12 12:20:01 -0300

    Update Formulas.cs

--- Formulas.cs
+++ Formulas.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Threading.Tasks;
+using YahooFinanceApi;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            // Retrieve historical data for TSLA from Yahoo Finance
+            var securities = await Yahoo.GetHistoricalAsync("TSLA", DateTime.Now.AddDays(-5), DateTime.Now, Period.Daily);
+
+            // Get the last available close price
+            var lastClose = securities[0].Close;
+
+            Console.WriteLine("Stock Name: TSLA");
+            Console.WriteLine("Last Close: $" + lastClose);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
